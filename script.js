294f6f25884ca48a42d18f9757a6789ef6b60464
Aaron11sudo <100597240+Aaron11sudo@users.noreply.github.com>
2022-02-28 21:28:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function browser_send_message(message_content) {
+    alert(message_content);
+}
+
+function go_to(url) {
+    window.open(url);
+}
+
+function destroy_id(id_name) {
+    document.getElementById(id_name).style.display="none";
+}
+
+(function(d){var s = d.createElement("script");s.setAttribute("data-account", "GxKkanEHrb");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)
