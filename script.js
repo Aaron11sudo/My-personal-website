0b515e364e6532397833cb8fa0f51e2fc2ee8a0d
Aaron11sudo <100597240+Aaron11sudo@users.noreply.github.com>
2022-03-03 13:02:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function go_to(url) {
 }
 
 function destroy_id(id_name) {
-    document.getElementById(id_name).style.display="none";
+    document.getElementById(id_name).style.display="none"
 }
-
 (function(d){var s = d.createElement("script");s.setAttribute("data-account", "GxKkanEHrb");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)
